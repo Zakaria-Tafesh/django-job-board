ab6df371ed9f46643bbdb6f1f0504c315775ad72
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2021-10-22 20:30:51 +0300

    db sqlite3

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'blog',
+    'contact',
+    'home',
+    'job',
 ]
 
 MIDDLEWARE = [
