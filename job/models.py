d341bdd5aa6ee0f5c9d30680b0a6a390daac7289
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2021-10-23 15:58:26 +0300

    models2

--- job/models.py
+++ job/models.py
@@ -1,29 +1,35 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 
 
 class Job(models.Model):
+    class JobTypeChoice(models.TextChoices):
+        FULL_TIME = 'FT', _('Full time')
+        PART_TIME = 'PT', _('Part time')
+
     title = models.CharField(max_length=1000)
     # location
-    FT = 'ft'
-    PT = 'pt'
-    job_type_choices = [
-        ('', 'Unknown Yet'),
-        (FT, 'Full time'),
-        (PT, 'Part time'),
-    ]
 
-    # job_choices = [
-    #     (Full_time, 'Full time'),
-    #     ('Part time', 'Part time')
+    # Old way
+    # FT = 'ft'
+    # PT = 'pt'
+    # job_type_choices = [
+    #     ('', 'Unknown Yet'),
+    #     (FT, 'Full time'),
+    #     (PT, 'Part time'),
     # ]
 
-    job_type = models.CharField(max_length=2, choices=job_type_choices, default=FT)
-    description = models.TextField(max_length=1000, default='')
+    # job_type = models.CharField(max_length=2, choices=job_type_choices, default=FT)
+    job_type = models.CharField(max_length=2, choices=JobTypeChoice.choices, default=JobTypeChoice.FULL_TIME)
+    description = models.TextField(max_length=1000, default='', blank=True)
     published_at = models.DateTimeField(auto_now=True)
     Vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     # category =
     experience = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
